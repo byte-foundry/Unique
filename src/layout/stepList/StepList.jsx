6092d12fe5267b9cad7161ed0094e0e051ad1be5
Julien <ibarretj@gmail.com>
2017-07-07 09:30:48 +0200

    pre-design step view

--- src/layout/stepList/StepList.jsx
+++ src/layout/stepList/StepList.jsx
@@ -6,9 +6,9 @@ import './StepList.css';
 
 const StepList = props => (
   <div className="StepList">
-    <Link key="template" to="/">Template choice</Link>
+    <Link key="template" to="/"><div>Template choice</div></Link>
     {props.steps.map((step, index) => (
-      <Link key={`step-${index + 1}`}to={`/template/${props.template}/${index + 1}`}>{step.title}</Link>
+      <Link key={`step-${index + 1}`}to={`/template/${props.template}/${index + 1}`}><div>{step.title}</div></Link>
     ))}
   </div>
 );
