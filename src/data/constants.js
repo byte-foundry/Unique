3d6b634012f183adedb5bd54a638b31d2308ebf4
Aldrian <ibarretj@gmail.com>
2018-01-26 10:58:56 +0100

    Fix production

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,6 +10,10 @@ export const GRAPHQL_PROTOTYPO_API = process.env.NODE_ENV === 'production'
 ? 'https://api.graph.cool/simple/v1/prototypo-new-dev'
 : 'https://api.graph.cool/simple/v1/prototypo';
 export const AUTH0_DOMAIN = 'unique-app.eu.auth0.com';
-export const AUTH0_API = 'http://localhost:3000';
+export const AUTH0_API = process.env.NODE_ENV === 'production'
+? 'http://localhost:3000'
+: 'http://peasy-poc.netlify.com';
 export const AUTH0_CLIENTID = 'i4KEy4mGBQIp4Yd55QC4Z1fa35qeIKN7';
-export const AUTH0_CALLBACKURL = 'http://localhost:3000/callback';
+export const AUTH0_CALLBACKURL = process.env.NODE_ENV === 'production'
+? 'http://localhost:3000/callback'
+: 'http://peasy-poc.netlify.com';
