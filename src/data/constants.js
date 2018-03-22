6b0b39a04d49c31fc2c6bc9acc43c5b3f659937a
Aldrian <ibarretj@gmail.com>
2018-03-22 18:37:30 +0100

    Set temporary all appId in dev

--- src/data/constants.js
+++ src/data/constants.js
@@ -21,16 +21,29 @@ export const AUTH0_CALLBACKURL =
     ? "http://peasy-poc.netlify.com/callback"
     : "http://localhost:3000/callback";
 export const BASE_PACK_PRICE = 15.0;
+// const AWS_URL = `https://${
+//   process.env.NODE_ENV === "production" ? "e4jpj60rk8" : "tc1b6vq6o8"
+// }.execute-api.eu-west-1.amazonaws.com/${
+//   process.env.NODE_ENV === "production" ? "prod" : "dev"
+// }`;
 const AWS_URL = `https://${
-  process.env.NODE_ENV === "production" ? "e4jpj60rk8" : "tc1b6vq6o8"
+  process.env.NODE_ENV === "production" ? "tc1b6vq6o8" : "tc1b6vq6o8"
 }.execute-api.eu-west-1.amazonaws.com/${
-  process.env.NODE_ENV === "production" ? "prod" : "dev"
+  process.env.NODE_ENV === "production" ? "dev" : "dev"
 }`;
 export const TWITTER_REQUEST_TOKEN_URL = `${AWS_URL}/auth/twitter/requestToken`;
+// export const FACEBOOK_APP_ID =
+//   process.env.NODE_ENV === "production" ? "360143951128760" : "569126220107317";
 export const FACEBOOK_APP_ID =
-  process.env.NODE_ENV === "production" ? "360143951128760" : "569126220107317";
+  process.env.NODE_ENV === "production" ? "569126220107317" : "569126220107317";
+// export const GOOGLE_CLIENT_ID = `498899515436-${
+//   process.env.NODE_ENV === "production"
+//     ? "aiq68iif29l3dh8pcrjgn8uvpht180vv"
+//     : "7c84imarpufkvf56olbpodnks3d3kg2p"
+// }.apps.googleusercontent.com`;
+
 export const GOOGLE_CLIENT_ID = `498899515436-${
   process.env.NODE_ENV === "production"
-    ? "aiq68iif29l3dh8pcrjgn8uvpht180vv"
+    ? "7c84imarpufkvf56olbpodnks3d3kg2p"
     : "7c84imarpufkvf56olbpodnks3d3kg2p"
 }.apps.googleusercontent.com`;
