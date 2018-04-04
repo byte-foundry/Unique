2b9bd6d1d9e54b5914682484f0f8a47a0c6fe773
Aldrian <ibarretj@gmail.com>
2018-04-04 17:04:07 +0200

    Alternate between tags

--- src/components/template/index.jsx
+++ src/components/template/index.jsx
@@ -1,24 +1,46 @@
 // @flow
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Template.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "./Template.css";
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
 }
-class Template extends React.Component {  
+class Template extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tags: props.font.tags
+    };
+    this.shuffleTags = this.shuffleTags.bind(this);
+  }
+  componentDidMount() {
+    this.shuffleTags();
+  }
+  shuffleTags() {
+    setInterval(() => {
+      let tags = this.state.tags;
+      shuffleArray(tags);
+      this.setState({ tags });
+    }, 3000);
+  }
   render() {
-    let tags = [...this.props.font.tags];
-    shuffleArray(tags);
+    let tags = [...this.state.tags];
     return (
       <div
-        key={`preset${this.props.font.variant.family.name}${this.props.font.variant.name}`}
+        key={`preset${this.props.font.variant.family.name}${
+          this.props.font.variant.name
+        }`}
         className={`Template ${this.props.selected ? "selected" : ""}`}
         onClick={this.props.onClick}
         onDoubleClick={this.props.onDoubleClick}
-        style={{ fontFamily: `'${this.props.font.variant.family.name}${this.props.font.variant.name}'` }}
+        style={{
+          fontFamily: `'${this.props.font.variant.family.name}${
+            this.props.font.variant.name
+          }'`
+        }}
       >
         {tags[0]}
         <p className="templateName">{`${tags[1]}, ${tags[2]}`}</p>
@@ -30,19 +52,19 @@ class Template extends React.Component {
 Template.propTypes = {
   font: PropTypes.shape({
     preset: PropTypes.string.isRequired,
-    variant: PropTypes.string.isRequired,
+    variant: PropTypes.string.isRequired
   }).isRequired,
   onClick: PropTypes.func.isRequired,
   onDoubleClick: PropTypes.func.isRequired,
   text: PropTypes.string,
   mostSelected: PropTypes.bool,
   isLoading: PropTypes.bool.isRequired,
-  selected: PropTypes.bool.isRequired,
+  selected: PropTypes.bool.isRequired
 };
 
 Template.defaultProps = {
-  text: 'Hamburgefonstiv - Abc 123',
-  mostSelected: false,
+  text: "Hamburgefonstiv - Abc 123",
+  mostSelected: false
 };
 
 export default Template;
