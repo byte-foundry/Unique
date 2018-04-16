ebccded373ee7e1227c0d21d656c3388afcc252d
Aldrian <ibarretj@gmail.com>
2018-04-16 15:02:42 +0200

    Fix - Checkout header margin and fixed back arrow

--- src/containers/checkout/index.jsx
+++ src/containers/checkout/index.jsx
@@ -125,19 +125,19 @@ class Checkout extends React.Component {
     return (
       <div className="Checkout">
         <div className="container">
-          <Back
-            className="icon-back"
-            onClick={() => {
-              this.props.goBack();
-            }}
-          />
           <h2>
             <FormattedMessage
               id="CheckoutView.title"
               defaultMessage="Create your Unique package:"
               description="Checkout view title"
             />
-          </h2>
+          </h2>          
+          <Back
+            className="icon-back"
+            onClick={() => {
+              this.props.goBack();
+            }}
+          />
           <div className="checkout-options">
             <Masonry breakPoints={[350]}>
               {this.state.selectedOptions
